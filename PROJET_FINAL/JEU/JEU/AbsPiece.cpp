cd5d9095e8fb375b5e0d4657bfe369c5692c6cf7
GabSawka <gabriel.sawka@polymtl.ca>
2022-04-24 13:15:44 -0400

    abs piece inline fct

--- PROJET_FINAL/JEU/JEU/AbsPiece.cpp
+++ PROJET_FINAL/JEU/JEU/AbsPiece.cpp
@@ -1,10 +1,5 @@
 #include "AbsPiece.h"
 
-bool AbsPiece::isAlive()
-{
-    return alive_;
-}
-
 AbsPiece::AbsPiece(bool blackTeam, Square position)
 {
     blackTeam_ = blackTeam;
@@ -13,7 +8,3 @@ AbsPiece::AbsPiece(bool blackTeam, Square position)
     alive_ = true;
 }
 
-Square AbsPiece::getPosition()
-{
-    return position_;
-}
