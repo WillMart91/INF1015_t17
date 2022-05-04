8512014815fb5bb541f280f6f240fdb391bee786
WillMart91 <Willmartin91@outlook.com>
2022-05-04 11:54:24 -0400

    fdsfs

--- PROJET_FINAL/ProjetFinal/Tile.h
+++ PROJET_FINAL/ProjetFinal/Tile.h
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-//QT INCLUDES
-=======
-<<<<<<< HEAD:PROJET_FINAL/tp6cleaning(2)/Button.h
-/*author: Gabriel Sawka and Wiliam Martin
-* date: 25 april
-* description: class that represents a single square on the visual 8 by 8 board. 
-It affects the color of the square and handles everything to do with the click and hover of buttons.
-*/
-
-
-=======
-//QT INCLIDES
->>>>>>> 80ed777dbea3a6feb634004eac5e005ece5d7509:PROJET_FINAL/ProjetFinal/Tile.h
->>>>>>> 0857624456b0f17f1008a8f5f533726e17e58459
 #include <QGraphicsRectItem>
 #include <QGraphicsSceneMouseEvent>
 #include <QBrush>
